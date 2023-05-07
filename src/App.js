8fa0cb32c79eb58da24bca074886c346e8099b7b
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2023-05-07 16:13:34 +0200

    Created weather app with fake data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  let weatherData = {
+    cityName: "Prague",
+    date: "Sat 17:35",
+    description: "Mostly cloudy",
+    humidity: 76,
+    windSpeed: 26,
+    currentTemperature: 4,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Weather container">
+      <div className="app-container">
+        <div className="row">
+          <div className="col-4">
+            <h1>{weatherData.cityName}</h1>
+            <h3>{weatherData.date}</h3>
+            <h4>{weatherData.description}</h4>
+            <ul>
+              <li>Humidity: {weatherData.humidity}</li>
+              <li>Wind: {weatherData.windSpeed} km/h</li>
+            </ul>
+          </div>
+          <div className="col-8 align-self-center">
+            <span>
+              <h2>
+                <span role="img" aria-label="cloudy">
+                  ⛅
+                </span>{" "}
+                {weatherData.currentTemperature}
+              </h2>
+            </span>
+            <span className="units">°C</span>
+          </div>
+        </div>
+      </div>
+      <p>Open-source code, by TerezaBa</p>
     </div>
   );
 }
