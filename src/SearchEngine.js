eefca6a771209674826f2fdbe57c6f58cc3560d1
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2023-05-07 18:44:04 +0200

    Added axios for weather info

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
+  const [query, setQuery] = useState("");
+
+  function displayTemp(response) {
+    alert(`${response.data.temperature.current}`);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let apiKey = "b36tedd42903o5c6c68a4a10b4b1953f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayTemp);
+  }
+
+  function handleQuery(event) {
+    setQuery(event.target.value);
+  }
+
   let form = (
     <div className="SearchEngine">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
             <input
               type="text"
               className="form-control"
               placeholder="Enter a city"
-              autofocus="on"
+              autoFocus={true}
+              onChange={handleQuery}
             />
           </div>
           <div className="col-3">
@@ -22,5 +42,10 @@ export default function SearchEngine() {
     </div>
   );
 
-  return form;
+  return (
+    <div className="Weather">
+      {form}
+      <p>{query}</p>
+    </div>
+  );
 }
